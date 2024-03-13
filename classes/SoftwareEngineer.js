6d7367e0d1cb3699ed87a505bfe594937ef99d76
Kelvin M <kelkm609@gmail.com>
2024-03-13 16:30:40 -0400

    initial commit

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,5 +1,18 @@
-class SoftwareEngineer {
+const { Employees } = require("./Employees");
 
+class SoftwareEngineer extends Employees {
+        #programmingLanguages;
+        constructor(programmingLanguages){
+            this.#programmingLanguages = []
+        };
+
+        getProgrammingLanguages(){
+            return this.#programmingLanguages;
+        }
+        
+        setProgrammingLanguages(language){
+            this.#programmingLanguages.push = language;
+        }
 
 
     
